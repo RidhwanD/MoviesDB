472e1bc8f1ed86644ae8438fbf782d5258d49ed7
RidhwanD <ridhwan.dewoprabowo@gmail.com>
2022-01-21 11:54:53 +0100

    Add genre model and serializer

--- MovieData/movies_api/models.py
+++ MovieData/movies_api/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
 # Create your models here.
-# class Genre(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=100, null=False)
-#
-#     def __str__(self):
-#         return self.name
+class Genre(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, null=False)
-    genres = models.CharField(max_length=200, null=False)
+    genres = models.ManyToManyField(Genre)
     rating = models.FloatField(null=True)
     duration = models.IntegerField(null=True)
     quality = models.CharField(max_length=10, null=True)
--- MovieData/movies_api/serializers.py
+++ MovieData/movies_api/serializers.py
@@ -1,24 +1,15 @@
 from rest_framework import serializers
-from .models import Movie
+from .models import Movie, Genre
 
-# class GenreSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Genre
-#         fields = ('id', 'name')
-#
-#     def create(self, validated_data):
-#         return Genre.objects.create(**validated_data)
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
 
 class MovieSerializer(serializers.ModelSerializer):
-    # genres = GenreSerializer(many=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
-
-    # def create(self, validated_data):
-    #     genres_data = validated_data.get('genres', None)
-    #     movie = Movie.objects.create(**validated_data)
-    #     for genre_data in genres_data:
-    #         movie.genres.add(genre_data)
-    #     return movie
